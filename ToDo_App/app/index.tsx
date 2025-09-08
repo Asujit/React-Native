5700329336d2e4d5cd418faa0b4ab289b002e198
Asujit <autisujit24@gmail.com>
2025-09-08 23:23:35 +0530

    8-9

--- ToDo_App/app/index.tsx
+++ ToDo_App/app/index.tsx
@@ -1,16 +1,25 @@
+import { Ionicons } from "@expo/vector-icons";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import Checkbox from "expo-checkbox";
+import { useState } from "react";
 import {
   FlatList,
-  Text,
-  View,
-  StyleSheet,
-  TouchableOpacity,
   Image,
-  TextInput
+  Keyboard,
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Ionicons } from "@expo/vector-icons";
-import Checkbox from "expo-checkbox";
 
+type ToDoType = {
+  id: number;
+  title: string;
+  isDne: boolean;
+};
 
 export default function Index() {
   const todoData = [
@@ -40,6 +49,25 @@ export default function Index() {
       isDne: false,
     },
   ];
+  const [todos, setTodo] = useState<ToDoType[]>(todoData);
+  const [todoText, setTodoText] = useState<string>("");
+
+  const addTodo = async() => {
+    try{
+    const newTodo = {
+      id: Math.random(),
+      title: todoText,
+      isDne: false
+    };
+    todos.push(newTodo);
+    setTodo(todos);
+    await AsyncStorage.setItem('my-todo', JSON.stringify(todos));
+    setTodoText('');
+    Keyboard.dismiss();
+  } catch (error){
+    console.log(error)
+  }
+}
 
   return (
     <SafeAreaView style={styles.container}>
@@ -63,34 +91,60 @@ export default function Index() {
         </TouchableOpacity>
       </View>
 
-          <View style={styles.search}>
-            <Ionicons name="search" size={20} color={"black"} />
-            <TextInput placeholder="Search" style={styles.bar} clearButtonMode="always"></TextInput>
-
-          </View>
+      <View style={styles.search}>
+        <Ionicons name="search" size={20} color={"black"} />
+        <TextInput
+          placeholder="Search"
+          style={styles.bar}
+          clearButtonMode="always"
+        ></TextInput>
+      </View>
 
       <FlatList
-        data={todoData}
+        data={[...todos].reverse()}
         keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => (
-          <View style={styles.todoContainer}>
-            <Checkbox value={item.isDne}/>
-            <Text style={[styles.todoText , item.isDne && {textDecorationLine : "line-through"}]}>{item.title}</Text>
-            <TouchableOpacity onPress={() => {alert("Deleted " + item.id)}}>
-            <Ionicons name="trash" size={20} color={"red"} />
-            </TouchableOpacity>
-          </View>
-        )}
+        renderItem={({ item }) => <TodoItem todo={item} />}
       />
-      <View style={styles.footer}>
-        <TextInput placeholder="Add New ToDo" style={styles.newTodoInput} />
-        <TouchableOpacity onPress={() =>{}} style={styles.addBtn}>
+      <KeyboardAvoidingView
+        style={styles.footer}
+        behavior="padding"
+        keyboardVerticalOffset={10}
+      >
+        <TextInput
+          placeholder="Add New ToDo"
+          value={todoText}
+          onChangeText={(text) => setTodoText(text)}
+          style={styles.newTodoInput}
+          autoCorrect={false}
+        />
+        <TouchableOpacity onPress={() => addTodo()} style={styles.addBtn}>
           <Ionicons name="add" size={30} color={"white"} />
         </TouchableOpacity>
-      </View>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 }
+
+const TodoItem = ({ todo }: { todo: ToDoType }) => (
+  <View style={styles.todoContainer}>
+    <Checkbox value={todo.isDne} />
+    <Text
+      style={[
+        styles.todoText,
+        todo.isDne && { textDecorationLine: "line-through" },
+      ]}
+    >
+      {todo.title}
+    </Text>
+    <TouchableOpacity
+      onPress={() => {
+        alert("Deleted " + todo.id);
+      }}
+    >
+      <Ionicons name="trash" size={20} color={"red"} />
+    </TouchableOpacity>
+  </View>
+);
 
 const styles = StyleSheet.create({
   container: {
@@ -109,51 +163,51 @@ const styles = StyleSheet.create({
     width: 40,
     borderRadius: 20,
   },
-  search :{
-    backgroundColor : "white",
+  search: {
+    backgroundColor: "white",
     flexDirection: "row",
     alignItems: "center",
     borderRadius: 10,
     paddingHorizontal: 20,
-    paddingVertical:10,
+    paddingVertical: 10,
     marginBottom: 20,
-    gap: 20
+    gap: 20,
   },
-  bar :{
-    flex : 1,
+  bar: {
+    flex: 1,
     fontSize: 16,
-    color : "black",
+    color: "black",
     // backgroundColor: "blue"
   },
-  todoContainer :{
+  todoContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
     padding: 15,
-    marginVertical:5,
-    backgroundColor: "white"
+    marginVertical: 5,
+    backgroundColor: "white",
   },
-  todoText : {
+  todoText: {
     color: "black",
-    fontSize: 16
+    fontSize: 16,
   },
-  footer :{
+  footer: {
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
-    gap : 15
+    gap: 15,
   },
-  newTodoInput :{
-    flex:1,
+  newTodoInput: {
+    flex: 1,
     backgroundColor: "white",
     padding: 15,
     borderRadius: 10,
-    fontSize : 16,
-    color: "black"
+    fontSize: 16,
+    color: "black",
   },
-  addBtn : {
+  addBtn: {
     backgroundColor: "gray",
-    padding : 10,
-    borderRadius: 10
-  }
+    padding: 10,
+    borderRadius: 10,
+  },
 });

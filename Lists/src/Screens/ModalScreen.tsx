236774627a9b17b6d65da5f4a247931ed5ead32a
Asujit <autisujit24@gmail.com>
2025-10-07 23:58:29 +0530

    MODAL

--- Lists/src/Screens/ModalScreen.tsx
+++ Lists/src/Screens/ModalScreen.tsx
@@ -1,15 +1,69 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Modal, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { useState } from 'react'
 
 export default function ModalScreen() {
 
-  
+  const [showModal, setShowModal] = useState(false);
 
   return (
-    <View>
+    <View style={styles.container}>
       <Text>ModalScreen</Text>
+      <TouchableOpacity style={styles.btn} onPress={() => setShowModal(!showModal)}>
+        <Text style={styles.btnText}>Open Modal</Text>
+      </TouchableOpacity>
+
+      <Modal
+      animationType='slide'
+      visible={showModal}
+      onRequestClose={() => setShowModal(false)}
+      transparent={true}
+      >
+        <View style={styles.modalBg}>
+          <View style={styles.Bg}>
+            <TouchableOpacity style={styles.btn} onPress={() => setShowModal(false)}>
+              <Text style={styles.btnText}>Close</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
     </View>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container :{
+    flex:1,
+    alignItems:"center",
+    padding: 20
+  },
+  btn : {
+    width: '100%',
+    borderWidth:0.5,
+    borderColor: "#fff",
+    backgroundColor:"#075a21ff",
+    padding: 20,
+    borderRadius: 10,
+    alignItems: "center",
+    marginVertical: 20
+  },
+  btnText : {
+    fontSize: 20,
+    fontWeight: "bold",
+    color : "#fff"
+  },
+  modalBg : {
+    justifyContent: "center",
+    alignItems:"center",
+    padding: 20,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    height: "100%"
+  },
+  Bg :{
+    width:"100%",
+    backgroundColor: "#bcbbbbff",
+    padding: 50,
+    borderRadius:20,
+    elevation:5,
+    shadowColor:"#000"
+  }
+})

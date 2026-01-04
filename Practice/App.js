11b7a8edb151a1a25bdafaaee713744e8f2f829d
Asujit <autisujit24@gmail.com>
2026-01-04 20:13:24 +0530

    new-Components

--- Practice/App.js
+++ Practice/App.js
@@ -9,11 +9,25 @@ import {
   ImageBackground,
   TouchableOpacity,
   TouchableHighlight,
-  Pressable
+  Pressable,
+  TextInput,
+  Button,
+  Alert,
+  Switch,
+  ActivityIndicator,
+  Modal
 } from "react-native";
 
 export default function App() {
   const [count, setCount] = useState(0);
+  const [text, setText] = useState('');
+  const [comment, setComment] = useState('')
+  const [enabled, setEnabled] = useState(false)
+  const [modal, setModal] = useState(false)
+
+  const toggle = () => {
+    setEnabled(previous => !previous)
+  }
 
   const DATA = [
     { id: 1, title: "First" },
@@ -30,6 +44,10 @@ export default function App() {
       <Text style={styles.itemText}>{item.title}</Text>
     </View>
   );
+
+  const btnPressed = () =>{
+    Alert.alert('Button Pressed')
+  }
   return (
     // <ScrollView style={styles.container}>
     //   <View style={styles.first}>
@@ -70,26 +88,84 @@ export default function App() {
     //   </ImageBackground>
     // </View>
 
+    // <View style={styles.container}>
+    //   <TouchableHighlight onPress={() => setCount(count + 1)} style={styles.btn} underlayColor={'#5044d3ff'}>
+    //     <Text style={styles.title}>Addition</Text>
+    //   </TouchableHighlight>
+
+    //   <TouchableOpacity onPress={() => setCount(count - 1)} style={styles.btn}>
+    //     <Text style={styles.title}>Substraction</Text>
+    //   </TouchableOpacity>
+
+    //   <Pressable onPress={() => setCount(count * 2)} style={styles.btn}>
+    //     <Text style={styles.title}>Multiplication</Text>
+    //   </Pressable>
+
+    //   <TouchableOpacity onPress={() => setCount(count / 2)} style={styles.btn}>
+    //     <Text style={styles.title}>Dividation</Text>
+    //   </TouchableOpacity>
+
+    //   <View>
+    //     <Text style={styles.count}>{count}</Text>
+    //   </View>
+    // </View>
+
     <View style={styles.container}>
-      <TouchableHighlight onPress={() => setCount(count + 1)} style={styles.btn} underlayColor={'#5044d3ff'}>
-        <Text style={styles.title}>Addition</Text>
-      </TouchableHighlight>
+      {/* <TextInput 
+      placeholder="Type here..."
+      value={text}
+      onChangeText={setText}
+      keyboardType="default"
+      // secureTextEntry={!}
+      maxLength={10}
+      style={styles.input}
+      />
 
-      <TouchableOpacity onPress={() => setCount(count - 1)} style={styles.btn}>
-        <Text style={styles.title}>Substraction</Text>
-      </TouchableOpacity>
+      <Text style={styles.itemText}>Current Text is {text}</Text>
 
-      <Pressable onPress={() => setCount(count * 2)} style={styles.btn}>
-        <Text style={styles.title}>Multiplication</Text>
+      <TextInput
+        value={comment}
+        onChangeText={setComment}
+        multiline={true}
+        numberOfLine={5}
+        placeholder="Write comment here.."
+        style={styles.input}
+      />
+
+      <Text style={styles.itemText}>Your Comment is {comment}</Text>
+
+      <Button
+        title='Press me'
+        onPress={btnPressed}
+      />
+
+      <Switch 
+      value={enabled}
+      onValueChange={toggle}
+      />
+
+      <ActivityIndicator /> */}
+
+      <Pressable 
+      onPress={() => setModal(true)}
+      style={styles.mb} 
+      >
+        <Text style={styles.modalText}>Open Modal</Text>
       </Pressable>
 
-      <TouchableOpacity onPress={() => setCount(count / 2)} style={styles.btn}>
-        <Text style={styles.title}>Dividation</Text>
-      </TouchableOpacity>
+      <Modal 
+        visible={modal}
+        animationType="slide"
+        onRequestClose={() => setModal(false)}
+      >
+        <View style={styles.mview}>
+          <Text style={styles.mText}>This is the Modal</Text>
+          <Pressable style={styles.mb} onPress={() => setModal(false)}>
+            <Text style={styles.modalText}>Close the Modal</Text>
+          </Pressable>
+        </View>
+      </Modal>
 
-      <View>
-        <Text style={styles.count}>{count}</Text>
-      </View>
     </View>
   );
 }
@@ -154,4 +230,36 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     fontSize: 24,
   },
+  input: {
+    borderWidth: 1,
+    borderColor: '#000',
+    paddingVertical: 10,
+    width: '100%'
+  },
+  mview:{
+    width: '70%',
+    height: '40%',
+    alignSelf: 'center',
+    justifyContent: 'center',
+    borderWidth:1,
+    borderRadius:10,
+    alignItems: 'center'
+  },
+  mText:{
+    fontWeight:"bold",
+    fontSize:20
+  },
+  mb:{
+    width:'40%',
+    height:'20%',
+    margin:20,
+    borderWidth:1,
+    borderRadius:10,
+    alignItems: 'center',
+    justifyContent:'center',
+    backgroundColor:'red'
+  },
+  modalText:{
+    color:'#fff'
+  }
 });
